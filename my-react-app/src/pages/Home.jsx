1f1d757a6f6384fca81858db726c819f554e1822
Werner Marx <werner.marx@outlook.com>
2024-10-21 19:48:47 -0600

    tailwind catalyst

---
+++ my-react-app/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { SidebarLayout } from '../components/sidebar-layout'
+
+const Home = () => {
+  return (
+    <SidebarLayout>
+      <h1 className="text-3xl font-bold">Welcome to the Home Page</h1>
+      {/* You can add more content here */}
+    </SidebarLayout>
+  );
+};
+
+export default Home;
